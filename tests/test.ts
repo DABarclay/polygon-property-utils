087973a4d9212fb42c4b257c37e383ae62ffa344
DABarclay <danielbarclay1996@gmail.com>
2024-07-29 18:49:47 -0300

    test: :test_tube: Add tests to fail and test for tangent line closest point

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { polygonCentroid, polygonArea, polygonInternalLabel } from "../src";
 
-import { test_cases } from "./test_obs";
+import { null_cases, test_cases } from "./test_obs";
 
 //let box_1_result = calculateVisualCentre(u);
 
@@ -16,5 +16,17 @@ for (let i = 0; test_cases.length > i; i++) {
     const box_1_result = polygonInternalLabel(test_cases[i].polygon);
 
     expect(box_1_result).toEqual(expect.arrayContaining(test_cases[i].centre));
+  });
+}
+
+for (let i = 0; null_cases.length > i; i++) {
+  test("Expect return null on area", () => {
+    const box_1_result = polygonArea(null_cases[i].polygon);
+    expect(box_1_result).toBe(0);
+  });
+  test("Expect return null for label location", () => {
+    const box_1_result = polygonInternalLabel(null_cases[i].polygon);
+
+    expect(box_1_result).toEqual(null);
   });
 }
